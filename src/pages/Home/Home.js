42b828ef90e40f2daa1524ed17abf6acdf834c41
puneet@0408 <punitvirmani725@gmail.com>
2024-06-23 15:43:11 +0530

    j

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import HomeBanner from "./HomeBanner/HomeBanner"
 import Topdiscount from "./topdiscount/TopDiscount";
 import Toprated from "./topRated/Toprated";
 import Featuredproduct from "./Featuredproduct/Featuredproduct";
-import Electronics from "./electronics/Electronics";
+import Electronics from "./electronics/electronics";
 import Dresses from "./dresses/Dresses";
 
 function Home() {
